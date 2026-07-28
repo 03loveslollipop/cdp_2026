460619858997ae927cb16d8f71f48df4c7d8711c
02loveslollipop <git@02labs.me>
2026-07-28 00:45:27 -0500

    Add project structure: mlops_pipeline with src, notebooks, modules, and setup files

---
+++ setup.bat
@@ -0,0 +1,5 @@
+@echo off
+REM Windows setup script
+echo Setting up environment...
+pip install -r requirements.txt
+echo Done.
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Unix setup script
+set -e
+pip install -r requirements.txt
+echo "Setup complete."
